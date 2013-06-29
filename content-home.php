fc0f573b39c1678d2b24123be5b30bfe543e4a1d
Hasin Hayder <hasin@leevio.com>
2013-06-30 02:09:05 +0600

    UI - Time Column

--- content-home.php
+++ content-home.php
@@ -2,9 +2,10 @@
 	<?php
 	while(have_posts()){ the_post(); ?>
 	<div class="feed">
-		<div class="icon"> * </div>
+		<div class="icon"> ☆ </div>
 		<div class="info"> <?php the_author( );?> </div>
 		<div class="content"> <?php the_title();?> </div>
+		<div class="meta"> <?php the_time();?> </div>
 		<div class="clear"></div>
 	</div>	
 	<?php } ?>
--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 	$(document).ready(function(){
 		var w = $(window).width();
 		var fw = w-280;
+		var cw = fw-20-100-20-220;
 		$(".feed").css("width",fw);
+		$(".feed .content").css("width",cw);
 	});
 }(jQuery));
