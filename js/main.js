60cc1d4f9007dc46ddd975b6c16e75e2a5e1ab24
Hasin Hayder <hasin@leevio.com>
2013-06-30 01:16:44 +0600

    jQuery Conflict Fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-$(function(){
-	var $=jQuery;
+(function($){
+	//var $=jQuery;
 	$(document).ready(function(){
 		var w = $(window).width();
 		var fw = w-280;
 		$(".feed").css("width",fw);
 	});
-	
-	
-
 }(jQuery));
