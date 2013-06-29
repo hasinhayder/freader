25e41b7531a1930134053210019fb7f687ec53cb
Hasin Hayder <hasin@leevio.com>
2013-06-30 03:07:48 +0600

    Header Buttons

---
+++ header-buttons.php
@@ -0,0 +1,5 @@
+<div class="header-buttons">
+	<input type="button" class="btn action" value="Refresh">
+	<input type="button" class="btn action" value="Unsubscribe">
+	<input type="button" class="btn action" value="Feed Settings">
+</div>
