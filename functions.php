a88b87786d76cfb919ec08bb822e1fc5a24bac2f
Hasin Hayder <hasin@leevio.com>
2013-06-29 20:11:28 +0600

    Boilerplate

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+add_filter("wp_enqueue_scripts","scripts_and_styles");
+function scripts_and_styles(){
+	
+	wp_register_script( "modernizer", "//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js", "jquery", null );
+	wp_register_script( "app-js", get_template_directory_uri(). "/js/main.js", "jquery", null );
+	
+	wp_enqueue_script( "jquery" );
+	wp_enqueue_script( "modernizer" );
+	wp_enqueue_script( "app-js" );
+
+	wp_enqueue_style( "app-style", get_template_directory_uri()."/css/app.css" );
+
+}
+
+show_admin_bar( false );
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+get_template_part( "header" );
+get_template_part( "footer" );
+?>
