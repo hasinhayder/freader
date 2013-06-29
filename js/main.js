26c401cdf00415034f22a69c38f2a127334a11be
Hasin Hayder <hasin@leevio.com>
2013-06-30 03:07:22 +0600

    Feeds Section Scroll Y

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 	//var $=jQuery;
 	$(document).ready(function(){
 		var w = $(window).width();
-		var fw = w-280;
-		var cw = fw-20-100-20-220;
-		$(".feed").css("width",fw);
-		$(".feed .content").css("width",cw);
+		var h = $(window).height();
+		var feed_width = w-280;
+		var content_width = feed_width-20-100-20-220;
+		var feeds_height = h-205;
+		$(".feed").css("width",feed_width);
+		$(".feeds").css("height",feeds_height);
+		$(".feed .content").css("width",content_width);
 	});
 }(jQuery));
