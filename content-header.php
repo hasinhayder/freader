aa5d98f977a3ebe95d8ebcd2a9545bcfc6668de9
Hasin Hayder <hasin@leevio.com>
2013-06-30 21:24:34 +0600

    Text Changed in Header

--- content-header.php
+++ content-header.php
@@ -1,6 +1,6 @@
 <header class="main">
 	<div class="logo">
 	<h1>Feed Reader</h1>
-	<h2>In Memories of Google Reader (October 7, 2005 - June 30, 2013)</h2>
+	<h2>In Memory of Google Reader (October 7, 2005 - July 1, 2013)</h2>
 	</div>
 </header>
